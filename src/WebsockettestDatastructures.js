14715d300fc40f70c481ccf24f74b91734c92e92
p.gurina <p.gurina@wellyes.ru>
2021-04-08 17:35:13 +0300

    Put back TestState.CONNECT_UPLOAD

--- src/WebsockettestDatastructures.js
+++ src/WebsockettestDatastructures.js
@@ -123,7 +123,8 @@ function RMBTTestThread(cyclicBarrier) {
          * Triggers the next state in the thread
          */
         triggerNextState: function() {
-            let states = [TestState.INIT, TestState.INIT_DOWN, TestState.PING, TestState.DOWN, TestState.INIT_UP, TestState.UP, TestState.END];
+            let states = [TestState.INIT, TestState.INIT_DOWN, TestState.PING,
+                TestState.DOWN, TestState.CONNECT_UPLOAD, TestState.INIT_UP, TestState.UP, TestState.END];
             if (this.state !== TestState.END) {
                 let nextState = states[states.indexOf(this.state) + 1];
                 _logger.debug(this.id + ": triggered state " + nextState);
